6a96943436a2c4c2a3931dfa2936e93454240765
Heather Clendenin <heather.r.clendenin@gmail.com>
2024-12-03 12:04:40 -0600

    Create Cactus_halLiftover.sh
    raw script for cactus alignment liftover

---
+++ 03_Variant_annotation&genomic_location/Cactus_halLiftover.sh
@@ -0,0 +1,2 @@
+module load cactus
+halLiftover /home/mdpllard/Cactus/heather_bears/steps-output/bears_alignment.hal Ursus_americanus_NCBI /home/mdpllard/Cactus/heather_bears/halLiftover/allVar_dupRemoved.txt Ursus_americanus_DNAZoo /home/mdpllard/Cactus/heather_bears/halLiftover/allVar_DNAZoo.bed --bedType 4
