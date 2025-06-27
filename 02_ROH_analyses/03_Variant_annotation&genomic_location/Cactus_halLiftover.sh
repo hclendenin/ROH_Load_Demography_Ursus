b3c5e3dc99fbde8de25c1541a9bc4eed71d99ef4
Heather Clendenin <heather.r.clendenin@gmail.com>
2025-06-27 13:01:16 -0500

    Delete 02_ROH_analyses/03_Variant_annotation&genomic_location directory

--- 02_ROH_analyses/03_Variant_annotation&genomic_location/Cactus_halLiftover.sh
+++
@@ -1,2 +0,0 @@
-module load cactus
-halLiftover /home/mdpllard/Cactus/heather_bears/steps-output/bears_alignment.hal Ursus_americanus_NCBI /home/mdpllard/Cactus/heather_bears/halLiftover/allVar_dupRemoved.txt Ursus_americanus_DNAZoo /home/mdpllard/Cactus/heather_bears/halLiftover/allVar_DNAZoo.bed --bedType 4
